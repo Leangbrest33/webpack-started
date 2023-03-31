59b14cbde8c40067e6a49a4fe6d8711404ffc735
Lean G.Brest <leangonbrest@gmail.com>
2023-03-31 12:54:17 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 
-import { saludar } from './js/componentes';
 import './styles.css'
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-const nombre = 'Leandro!!!';
 
-saludar( nombre );
+export const todoList = new TodoList(); 
+
+todoList.todos.forEach( crearTodoHtml );
+
+// const newTodo = new Todo( 'Aprender Java Script ');
+// todoList.nuevoTodo( newTodo );
+
+// console.log( 'todos', todoList.todos );
